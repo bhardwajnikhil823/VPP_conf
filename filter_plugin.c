913d584901ff5b711eba2ffb1daf7e214f9e443d
bhardwajnikhil823 <97446845+bhardwajnikhil823@users.noreply.github.com>
2025-03-23 19:30:35 +0530

    Add files via upload

---
+++ filter_plugin.c
@@ -0,0 +1,127 @@
+/*
+ * filter_plugin.c - filter plugin for tracing incoming ICMP packet lengths.
+ *
+ * Copyright (c) <current-year> <your-organization>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vnet/vnet.h>
+#include <vnet/plugin/plugin.h>
+#include <filter_plugin/filter_plugin.h>
+#include <vlibapi/api.h>
+#include <vlibmemory/api.h>
+#include <vpp/app/version.h>
+#include <stdbool.h>
+
+#include <filter_plugin/filter_plugin.api_enum.h>
+#include <filter_plugin/filter_plugin.api_types.h>
+
+#define REPLY_MSG_ID_BASE fmp->msg_id_base
+#include <vlibapi/api_helper_macros.h>
+
+filter_plugin_main_t filter_plugin_main;
+
+/* We provide a simple setup_message_id_table here so that our API messages get a unique base.
+   (This is analogous to what mss_clamp does.) */
+u32 setup_message_id_table(void) {
+    static u32 next_message_id = 1;
+    return next_message_id++;
+}
+
+/* Action function to enable/disable the plugin feature on a given interface.
+   It attaches our node into the ip4-unicast arc so that ICMP packets can be observed. */
+int filter_plugin_enable_disable (filter_plugin_main_t * fmp, u32 sw_if_index, int enable_disable) {
+  vnet_sw_interface_t * sw;
+  int rv = 0;
+
+  if (pool_is_free_index(fmp->vnet_main->interface_main.sw_interfaces, sw_if_index))
+    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
+
+  sw = vnet_get_sw_interface(fmp->vnet_main, sw_if_index);
+  if (sw->type != VNET_SW_INTERFACE_TYPE_HARDWARE)
+    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
+
+  /* Attach our filter_plugin node into the ip4-unicast arc on this interface */
+  vnet_feature_enable_disable("ip4-unicast", "filter_plugin", sw_if_index, enable_disable, NULL, 0);
+
+  return rv;
+}
+
+/* CLI command function */
+static clib_error_t * filter_plugin_enable_disable_command_fn (vlib_main_t * vm,
+                                   unformat_input_t * input,
+                                   vlib_cli_command_t * cmd) {
+  filter_plugin_main_t * fmp = &filter_plugin_main;
+  u32 sw_if_index = 1;
+  int enable_disable = 1;
+  int rv;
+
+  while (unformat_check_input(input) != UNFORMAT_END_OF_INPUT) {
+    if (unformat(input, "disable"))
+      enable_disable = 0;
+    else if (unformat(input, "%U", unformat_vnet_sw_interface, fmp->vnet_main, &sw_if_index))
+      ;
+    else
+      break;
+  }
+
+  if (sw_if_index == ~0)
+    return clib_error_return (0, "Please specify an interface...");
+
+  rv = filter_plugin_enable_disable(fmp, sw_if_index, enable_disable);
+  if (rv)
+    return clib_error_return (0, "filter_plugin_enable_disable returned %d", rv);
+
+  return 0;
+}
+
+VLIB_CLI_COMMAND (filter_plugin_enable_disable_command, static) = {
+  .path = "filter_plugin enable-disable",
+  .short_help = "filter_plugin enable-disable <interface-name> [disable]",
+  .function = filter_plugin_enable_disable_command_fn,
+};
+
+/* API message handler (if needed; here commented out)
+static void vl_api_filter_plugin_enable_disable_t_handler(vl_api_filter_plugin_enable_disable_t *mp) {
+  vl_api_filter_plugin_enable_disable_reply_t * rmp;
+  filter_plugin_main_t * fmp = &filter_plugin_main;
+  int rv;
+  rv = filter_plugin_enable_disable(fmp, ntohl(mp->sw_if_index), (int)(mp->enable_disable));
+  REPLY_MACRO(VL_API_FILTER_PLUGIN_ENABLE_DISABLE_REPLY);
+}
+*/
+
+/* Initialization function */
+static clib_error_t * filter_plugin_init (vlib_main_t * vm) {
+  filter_plugin_main_t * fmp = &filter_plugin_main;
+  clib_error_t * error = 0;
+  fmp->vlib_main = vm;
+  fmp->vnet_main = vnet_get_main();
+  fmp->msg_id_base = setup_message_id_table();
+  return error;
+}
+VLIB_INIT_FUNCTION (filter_plugin_init);
+
+VNET_FEATURE_INIT (filter_plugin, static) =
+{
+  .arc_name = "ip4-unicast",
+  .node_name = "filter_plugin",
+  .runs_before = VNET_FEATURES ("ip4-lookup"),
+};
+
+/* Plugin registration */
+VLIB_PLUGIN_REGISTER () = {
+  .version = VPP_BUILD_VER,
+  .description = "filter_plugin: Traces incoming ICMP packet lengths",
+};
+
---
+++ node.c
@@ -0,0 +1,117 @@
+/*
+ * node.c - filter_plugin node for tracing incoming ICMP packets.
+ *
+ * This node is inserted into the ip4-unicast arc so that it can inspect IPv4 packets.
+ * When an IPv4 packet is received and its protocol is ICMP, it logs the total IP length.
+ *
+ * Copyright (c) <current-year> <your-organization>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vlib/vlib.h>
+#include <vnet/vnet.h>
+#include <vnet/ip/ip.h>
+#include <vnet/ip/ip4.h>
+#include <vlib/unix/plugin.h>
+#include <filter_plugin/filter_plugin.h>
+
+/* Trace structure for our node */
+typedef struct {
+  u32 sw_if_index;
+  u32 next_index;
+} filter_plugin_trace_t;
+
+/* Packet trace format function */
+static u8 * format_filter_plugin_trace (u8 * s, va_list * args) {
+  CLIB_UNUSED (vlib_main_t * vm) = va_arg (*args, vlib_main_t *);
+  CLIB_UNUSED (vlib_node_t * node) = va_arg (*args, vlib_node_t *);
+  filter_plugin_trace_t * t = va_arg (*args, filter_plugin_trace_t *);
+  s = format (s, "FILTER_PLUGIN: sw_if_index %d, next index %d", t->sw_if_index, t->next_index);
+  return s;
+}
+
+/* We define only one “next” (drop) for simplicity */
+typedef enum {
+  FILTER_PLUGIN_NEXT_DROP,
+  FILTER_PLUGIN_NEXT_FORWARD,
+  FILTER_PLUGIN_N_NEXT,
+} filter_plugin_next_t;
+
+/* Main processing function. For each packet, if the packet is IPv4 and its protocol is ICMP,
+   log the IP total length using vlib_log_debug(0, …). */
+always_inline uword filter_plugin_inline (vlib_main_t * vm,
+                                          vlib_node_runtime_t * node,
+                                          vlib_frame_t * frame,
+                                          int is_trace) {
+  u32 n_left_from, * from;
+  vlib_buffer_t * bufs[VLIB_FRAME_SIZE], **b;
+  u16 nexts[VLIB_FRAME_SIZE], *next;
+  from = vlib_frame_vector_args (frame);
+  n_left_from = frame->n_vectors;
+  vlib_get_buffers (vm, from, bufs, n_left_from);
+  b = bufs;
+  next = nexts;
+
+  while (n_left_from > 0) {
+    next[0] = FILTER_PLUGIN_NEXT_DROP; // default to drop (or forward as appropriate)
+    if (b[0]->current_length >= sizeof(ip4_header_t)) {
+      ip4_header_t * ip = (ip4_header_t *) vlib_buffer_get_current(b[0]);
+      if (((ip->ip_version_and_header_length >> 4) == 4) &&
+          (ip->protocol == IP_PROTOCOL_ICMP)) {
+        u16 icmp_len = clib_net_to_host_u16(ip->length);
+        vlib_log_debug(0, "ICMP packet received, length: %u bytes", icmp_len);
+      }
+    }
+    if (is_trace && (b[0]->flags & VLIB_BUFFER_IS_TRACED)) {
+      filter_plugin_trace_t * t = vlib_add_trace(vm, node, b[0], sizeof(*t));
+      t->next_index = next[0];
+      t->sw_if_index = vnet_buffer(b[0])->sw_if_index[VLIB_RX];
+    }
+    b++;
+    next++;
+    n_left_from--;
+  }
+  vlib_buffer_enqueue_to_next (vm, node, from, nexts, frame->n_vectors);
+  return frame->n_vectors;
+}
+
+/* Node function */
+VLIB_NODE_FN (filter_plugin_node) (vlib_main_t * vm,
+                                     vlib_node_runtime_t * node,
+                                     vlib_frame_t * frame) {
+  int trace_enabled = (node->flags & VLIB_NODE_FLAG_TRACE) ? 1 : 0;
+  return filter_plugin_inline(vm, node, frame, trace_enabled);
+}
+
+
+VLIB_REGISTER_NODE (filter_plugin_node) = {
+  .name = "filter_plugin",
+  .vector_size = sizeof(u32),
+  .format_trace = format_filter_plugin_trace,
+  .type = VLIB_NODE_TYPE_INTERNAL,
+  .n_next_nodes = FILTER_PLUGIN_N_NEXT,
+  .next_nodes = {
+    [FILTER_PLUGIN_NEXT_FORWARD] = "interface-output",
+    [FILTER_PLUGIN_NEXT_DROP] = "error-drop",
+  },
+};
+// #endif /* CLIB_MARCH_VARIANT */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
+
